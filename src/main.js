47b3eac5cff6253eb361dfa64ae890d5c0803110
Rollingegg <1244797125@qq.com>
2020-02-26 08:20:27 +0800

    增加login页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import VueResource from 'vue-resource'
+import router from './router.js'
 import App from './App.vue'
 
+Vue.use(ElementUI);
+Vue.use(VueResource);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+    router,
 }).$mount('#app')
