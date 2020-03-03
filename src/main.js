cb3bf5e2f82824bf1cc177d20dd0e45d75341a93
Rollingegg <1244797125@qq.com>
2020-03-03 15:29:42 +0800

    重新用@vue/cli脚手架初始化项目

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import VueResource from 'vue-resource'
-import router from './router.js'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
+Vue.config.productionTip = false;
 Vue.use(ElementUI);
-Vue.use(VueResource);
-Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-    router,
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
