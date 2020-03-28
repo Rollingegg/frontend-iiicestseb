c604a53210a11d061ec9805295534bd47f421e53
Rollingegg <1244797125@qq.com>
2020-03-29 04:49:57 +0800

    【更新】替换上传页面
    1. 天坑上传组件（再也不会用了）
    2. 天坑表格组件

--- src/views/excel/Excel.vue
+++ src/views/excel/Excel.vue
@@ -1,242 +1,227 @@
 <template>
-    <div style="width: 100%">
-        <div class="option-area">
-            <a-upload
-                class="upload-area"
-                :fileList="fileList"
-                :remove="handleRemove"
-                :disabled="fileList.length === 1"
-                :beforeUpload="beforeUpload">
-                <a-button>
-                    <a-icon type="upload"/>
-                    选择.csv文件
-                </a-button>
-            </a-upload>
-            <div class="button-area">
-                <a-button type="primary" @click="downloadTemplate" style="margin-right: .5rem">
-                    模板下载
-                </a-button>
-                <a-button
-                    @click="handleUpload"
-                    :disabled="fileList.length === 0"
-                    :loading="uploading">
-                    {{uploading ? '导入中' : '导入csv' }}
-                </a-button>
-            </div>
-        </div>
-        <a-card :bordered="false" class="card-area">
-            <!-- 表格区域 -->
-            <a-table ref="TableInfo"
-                     :rowKey="record =>record.id"
-                     :columns="columns"
-                     :dataSource="dataSource"
-                     :pagination="pagination"
-                     :loading="loading"
-                     @change="handleTableChange"
-                     :scroll="{ x: 900 }">
-            </a-table>
-        </a-card>
-        <import-result
-            @close="handleClose"
-            :importData="importData"
-            :errors="errors"
-            :times="times"
-            :importResultVisible="importResultVisible">
-        </import-result>
+  <div style="width: 100%">
+    <div class="option-area">
+      <el-upload
+        class="upload-area"
+        action="string"
+        ref="uploadAction"
+        :on-change="handleChange"
+        :fileList="fileList"
+        :on-remove="handleRemove"
+        :http-request="handleUpload"
+        :auto-upload="false"
+      >
+        <el-button slot="trigger" size="medium" icon="el-icon-upload2">选择.json文件</el-button>
+        <el-button
+          size="medium"
+          @click="submitUpload('uploadAction')"
+          :disabled="hasSelected"
+          :loading="uploading"
+        >{{uploading ? '导入中' : '导入json' }}</el-button>
+        <div slot="tip" class="el-upload__tip">请按照预定的模板json文件格式上传，且每次只能上传一个文件</div>
+      </el-upload>
+      <div class="button-area">
+        <el-button
+          size="medium"
+          type="primary"
+          @click="downloadTemplate"
+          style="margin-right: .5rem"
+        >模板下载</el-button>
+      </div>
     </div>
+    <div class="card-area">
+      <!-- 表格区域 -->
+      <el-table :data="tableData" style="width: 100%" height="250">
+        <el-table-column fixed prop="title" label="论文标题" width="300"></el-table-column>
+        <el-table-column prop="id" label="论文编号" width="50"></el-table-column>
+        <el-table-column prop="conferenceId" label="会议编号" width="50"></el-table-column>
+        <el-table-column prop="publisher" label="发布机关" width="50"></el-table-column>
+        <el-table-column prop="pdfUrl" label="pdf链接" :show-overflow-tooltip="true" width="150"></el-table-column>
+        <el-table-column prop="publicationTitle" label="刊物名称" :show-overflow-tooltip="true" width="300"></el-table-column>
+        <el-table-column prop="citationCountPaper" label="引用数" width="50"></el-table-column>
+        <el-table-column prop="citationCountPatent" label="被引数" width="50"></el-table-column>
+        <el-table-column prop="startPage" label="起始页" width="50"></el-table-column>
+        <el-table-column prop="endPage" label="结束页" width="50"></el-table-column>
+      </el-table>
+        <el-pagination
+        v-if="dataSource.length"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage"
+        :page-size="size"
+        layout="total, prev, pager, next, jumper"
+        :total="dataSource.length"
+        ></el-pagination>
+    </div>
+    <import-result
+      @close="handleClose"
+      :importData="importData"
+      :errors="errors"
+      :times="times"
+      :importResultVisible="importResultVisible"
+    ></import-result>
+  </div>
 </template>
 <script>
-    import ImportResult from './ImportResult';
-
-    export default {
-        components: {ImportResult},
-        data () {
-            return {
-                fileList: [],
-                importData: [],
-                times: '',
-                errors: [],
-                uploading: false,
-                importResultVisible: false,
-                dataSource: [],
-                paginationInfo: null,
-                pagination: {
-                    pageSizeOptions: ['10', '20', '30', '40', '100'],
-                    defaultCurrent: 1,
-                    defaultPageSize: 10,
-                    showQuickJumper: true,
-                    showSizeChanger: true,
-                    showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
-                },
-                loading: false
-            };
-        },
-        computed: {
-            columns () {
-                return [
-                    {
-                        title: '发布标题',
-                        dataIndex: 'publicationTitle',
-                        customRender: (text) => {
-                            if (text.length > 10) {
-                                text = text.substring(0, 8) + '...';
-                            }
-                            return text;
-                        }
-                    }, {
-                        title: '发布编号',
-                        dataIndex: 'publisherId'
-                    }, {
-                        title: '会议编号',
-                        dataIndex: 'conferenceId'
-                    }, {
-                        title: 'pdf链接',
-                        dataIndex: 'pdfLink',
-                        customRender: (text) => {
-                            if (text.length > 30) {
-                                text = text.substring(0, 28) + '...';
-                            }
-                            return text;
-                        }
-                    }, {
-                        title: 'doi',
-                        dataIndex: 'doi'
-                    }, {
-                        title: '标题',
-                        dataIndex: 'paperTitle',
-                        customRender: (text) => {
-                            if (text.length > 30) {
-                                text = text.substring(0, 28) + '...';
-                            }
-                            return text;
-                        }
-                    }, {
-                        title: '摘要',
-                        dataIndex: 'paperAbstract',
-                        customRender: (text) => {
-                            if (text.length > 30) {
-                                text = text.substring(0, 28) + '...';
-                            }
-                            return text;
-                        }
-                    }, {
-                        title: '引用数',
-                        dataIndex: 'referenceCount'
-                    }, {
-                        title: '被引数',
-                        dataIndex: 'citationCount'
-                    }, {
-                        title: '发布时间',
-                        dataIndex: 'publicationYear',
-                        customRender: (text) => {
-                            return text.substr(0, 4);
-                        }
-                    }, {
-                        title: '开始页',
-                        dataIndex: 'startPage'
-                    }, {
-                        title: '终了页',
-                        dataIndex: 'endPage'
-                    }, {
-                        title: '认定机构',
-                        dataIndex: 'documentIdentifier'
-                    }];
-            }
-        },
-        mounted () {
-        },
-        methods: {
-            handleClose () {
-                this.importResultVisible = false;
-            },
-            downloadTemplate () {
-                window.open(process.env.BASE_API + '/admin/paper/StandardJSON');
-            },
-            handleRemove (file) {
-                if (this.uploading) {
-                    this.$message.warning('文件导入中，请勿删除');
-                    return;
-                }
-                const index = this.fileList.indexOf(file);
-                const newFileList = this.fileList.slice();
-                newFileList.splice(index, 1);
-                this.fileList = newFileList;
-            },
-            beforeUpload (file) {
-                const isCSV = file.type === 'application/vnd.ms-excel';
-                if (!isCSV) {
-                    this.$message({
-                        showClose: true,
-                        message: 'CSV ONLY!',
-                        type: 'warning'
-                    });
-                } else {
-                    this.fileList = [...this.fileList, file];
-                }
-                return false;
-            },
-            handleUpload () {
-                const {fileList} = this;
-                const formData = new FormData();
-                formData.append('file', fileList[0]);
-                this.uploading = true;
-                let timeStart = new Date();
-                this.$upload('statistics/uploadCSV', formData).then((r) => {
-                    this.times = (new Date() - timeStart) / 1000;
-                    if (r.data.status) {
-                        this.errors = r.data.result.errors;
-                        this.importData = r.data.result.papers;
-                        this.importResultVisible = true;
-                        this.fetch();
-                    } else {
-                        this.importData = [];
-                        this.$message({
-                            showClose: true,
-                            message: r.data.result,
-                            type: 'warning'
-                        });
-                    }
-                    this.uploading = false;
-                    this.fileList = [];
-                }).catch((r) => {
-                    console.error(r);
-                    this.uploading = false;
-                    this.fileList = [];
-                });
-            },
-            handleTableChange (pagination, filters, sorter) {
-                this.paginationInfo = pagination;
-                // this.fetch();
-            },
-            fetch (params = {}) {
-                this.loading = true;
-                this.loading = false;
-                this.dataSource = this.importData;
-            }
-        }
+import ImportResult from "./ImportResult";
+export default {
+  components: { ImportResult },
+  data() {
+    return {
+      uploadUrl: process.env.BASE_API + "/admin/paper/StandardJSON",
+      replace: true,
+      msgPromise: Promise.resolve(),
+      hasSelected: true,
+      fileList: [],
+      importData: [],
+      times: "",
+      errors: [],
+      uploading: false,
+      importResultVisible: false,
+      dataSource: [],
+      tableData: [],
+      paginationInfo: null,
+      currentPage: 1,
+      size: 20,
+      loading: false
     };
+  },
+  methods: {
+    handleChange(file, fileList) {
+      //这表示是添加行为
+      // console.log(`准备修改文件${file.raw.name}, 此时文件列表长度为${fileList.length}`);
+      // console.log(`此时this.fileList的长度为${this.fileList.length}`);
+      if (file.status === "ready") {
+        this.hasSelected = false;
+        console.log(`添加了文件${file.raw.name}`);
+        let myFile = file.raw;
+        const type = myFile.type.toLowerCase();
+        const isJSON = type === "application/json";
+        if (!isJSON) {
+          fileList.pop();
+          this.handleMsg("JSON ONLY!", "warning");
+          return;
+        }
+        const fileIndex = fileList.indexOf(file);
+        if (fileIndex > 0) {
+          if (this.replace) {
+            fileList[0] = file;
+            fileList.pop();
+            this.handleMsg(`${myFile.name}文件重复，已覆盖该文件`, "warning");
+          } else {
+            fileList.pop();
+            this.handleMsg(`${myFile.name}文件重复，未覆盖该文件`, "warning");
+          }
+        }
+      }
+    },
+    handleClose() {
+      this.importResultVisible = false;
+    },
+    downloadTemplate() {
+      window.open(this.uploadUrl);
+    },
+    handleRemove(file) {
+      if (this.uploading) {
+        this.$message.warning("文件导入中，请勿删除");
+        return;
+      }
+      this.hasSelected = true;
+      const index = this.fileList.indexOf(file);
+      const newFileList = this.fileList.slice();
+      newFileList.splice(index, 1);
+      this.fileList = newFileList;
+    },
+    submitUpload(uploadName) {
+      this.$refs[uploadName].submit();
+    },
+    handleUpload(content) {
+      // const {fileList} = this;
+      console.log(content.file); // 已选择的文件
+      const formData = new FormData();
+      formData.append("file", content.file);
+      this.uploading = true;
+      let timeStart = new Date();
+      this.$upload("admin/paper/uploadJSON", formData)
+        .then(r => {
+          this.times = (new Date() - timeStart) / 1000;
+          if (r.data.status) {
+            this.errors = r.data.result.errorLogs;
+            this.importData = r.data.result.papers;
+            this.fetch();
+            setTimeout(()=>{
+                this.importResultVisible = true;
+            },500);
+          } else {
+            this.importData = [];
+            this.handleMsg(r.data.result, "error");
+          }
+          this.uploading = false;
+          this.fileList = [];
+        })
+        .catch(r => {
+          console.error(r);
+          this.uploading = false;
+          this.fileList = [];
+        });
+      this.hasSelected = true;
+    },
+    fetch() {
+      this.loading = true;
+      this.loading = false;
+      this.dataSource = this.importData;
+      this.handleCurrentChange(this.currentPage);
+    },
+    handleMsg(message, type = "info") {
+      const duration = 4000;
+      const showClose = true;
+      //一定要this.msgPromise = XXX才有用，这样后调用的才会.then接到后面
+      //Promise.then().then()和Promise.then(); Promise.then();是不一样的，前者是真串行
+      this.msgPromise = this.msgPromise.then(() => {
+        this.$message({ message, type, duration, showClose });
+      });
+    },
+    paging(size, current) {
+      const tableList = Array(this.dataSource);
+      const tablePush = [];
+      tableList.forEach((item, index) => {
+        if (size * (current - 1) <= index && index <= size * current - 1) {
+          tablePush.push(item);
+        }
+      });
+      return tablePush;
+    },
+    // 页码改变事件
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      this.tableData = this.paging(this.size, val);
+      console.log(this.tableData[0]);
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    @import "../../../static/less/Common";
+@import "../../../static/less/Common";
 
-    .option-area {
-        display: inline-block;
-        width: 100%;
-        padding: 0 .9rem;
-        margin: .5rem 0;
+.option-area {
+  display: inline-block;
+  width: 100%;
+  padding: 0 0.9rem;
+  margin: 0.5rem 0;
 
-        .upload-area {
-            display: inline;
-            float: left;
-            width: 50%
-        }
+  .upload-area {
+    float: left;
+    width: 30%;
+  }
 
-        .button-area {
-            margin-left: 1rem;
-            display: inline;
-            float: right;
-            width: 30%;
-        }
-    }
+  .button-area {
+    width: 30%;
+    margin-left: 1rem;
+    float: right;
+  }
+}
+.card-area {
+  padding: 0 0.9rem;
+  margin: 0.5rem 0;
+}
 </style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -152,7 +152,7 @@
                 this.$message.warning('暂未开发');
             },
             handleUsernameCheck (name) {
-                return !!(name.length && name.length < 20 && name.length > 4);
+                return !!(name.length && name.length < 21 && name.length > 3 && name.indexOf(' ')==-1);
             },
             handlePasswordLevel (value) {
                 if (value.length < 6 || value.length > 20) {
@@ -169,7 +169,7 @@
                 if (/[^0-9a-zA-Z_]/.test(value)) {
                     level++;
                 }
-                return level >= 2;
+                return level >= 1;
             },
             handleTabsChange (tab,event) {
                 // this.activeKey = val;
