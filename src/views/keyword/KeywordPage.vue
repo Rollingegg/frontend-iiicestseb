2a9d13796c01475251d7d3e766d9e9f118cbf14f
dwxh-mi <171250507@smail.nju.edu.cn>
2020-04-05 16:54:14 +0800

    关键词页面相关学者生效

--- src/views/keyword/KeywordPage.vue
+++ src/views/keyword/KeywordPage.vue
@@ -1,128 +1,190 @@
 <template>
-  <div class="main-container">
-    <el-row :gutter="20">
-      <el-col :md="16" class="domain-content">
-      <div class="domain-title">{{researchDomain}}</div>
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">领域描述</div>
-        <div class="domain-description">
-          {{domainDesciption}}
-        </div>
-      </el-card>
-      
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">活跃学者</div>
-        <div class="info-infinite-container">
-        <div >
-          活跃学者统计图
-        </div>
-        </div>
-      </el-card>
-      
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">活跃机构</div>
-        <div class="info-infinite-container">
-        <div >
-          活跃机构统计图
-        </div>
-        </div>
-      </el-card>
-      
-      <component v-if="currentTab!==null" :is="currentTab" :type="type" :keyword="keyword"></component>
-      </el-col>
-      <el-col :md="8" class="domain-recommend">
-        
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">论文发表</div>
-        <div class="info-infinite-container">
-          论文发表统计图
-        </div>
-      </el-card>
+    <div class="main-container">
+        <el-row :gutter="20">
+            <el-col :md="16" class="domain-content">
+                <div class="domain-title">{{researchDomain}}</div>
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">领域描述</div>
+                    <div class="domain-description">
+                        {{domainDesciption}}
+                    </div>
+                </el-card>
 
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">相关领域</div>
-        <div class="info-infinite-container">
-          与其相关的研究领域
-        </div>
-      </el-card>
-      
-      <el-card class="info-container">
-        <div slot="header" class="card-head-title">推荐会议</div>
-        <div class="info-infinite-container">
-          与该研究领域有较深关系的会议列表
-        </div>
-      </el-card>
-      </el-col>
-    </el-row>
-  </div>
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">活跃学者</div>
+                    <el-table :data="relativeAuthors"
+                              style="width: 100%"
+                              @cell-click="openAuthorOrAffiliation"
+                              :cell-style="cellStyle">
+                        <el-table-column width="40">
+                            <el-icon class="el-icon-user"/>
+                        </el-table-column>
+                        <el-table-column width="250" prop="name" label="作者"/>
+                        <el-table-column prop="affiliationName" label="所在机构"/>
+                    </el-table>
+                </el-card>
+
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">活跃机构</div>
+                    <div class="info-infinite-container">
+                        <div>
+                            活跃机构统计图
+                        </div>
+                    </div>
+                </el-card>
+
+                <component v-if="currentTab!==null" :is="currentTab" :type="type" :keyword="keyword"></component>
+            </el-col>
+            <el-col :md="8" class="domain-recommend">
+
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">论文发表</div>
+                    <div class="info-infinite-container">
+                        论文发表统计图
+                    </div>
+                </el-card>
+
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">相关领域</div>
+                    <div class="info-infinite-container">
+                        与其相关的研究领域
+                    </div>
+                </el-card>
+
+                <el-card class="info-container">
+                    <div slot="header" class="card-head-title">推荐会议</div>
+                    <div class="info-infinite-container">
+                        与该研究领域有较深关系的会议列表
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-import PaperList from '../author/PaperListPage';
-export default {
-    name: 'KeywordPage',
-    data () {
-      return {
-        currentTab: PaperList,
-        activeName: 'overview',
-        keywordId: '',
-        type:'term',
-        researchDomain: 'Artificial Intelligence System',
-        domainDesciption: 'Artificial Intelligence System (AIS) was a distributed computing project undertaken by Intelligence Realm, Inc. with the long-term goal of simulating the human brain in real time, complete with artificial consciousness and artificial general intelligence. They claimed to have found, in research, the "mechanisms of knowledge representation in the brain which is equivalent to finding artificial intelligence", before moving into the developmental phase.'
-      };
-    },
-    computed: {
-      keyword() {
-      return { type: this.type, term: this.researchDomain };
-    }
-    },
-    methods: {
-      
-    },
-    created () {
-        this.keywordId = this.$route.query.id;
-    }
-};
+    import PaperList from '../author/PaperListPage';
+
+    export default {
+        name: 'KeywordPage',
+        data () {
+            return {
+                currentTab: PaperList,
+                activeName: 'overview',
+                keywordId: '',
+                type: 'term',
+                researchDomain: 'Artificial Intelligence System',
+                domainDesciption: 'Artificial Intelligence System (AIS) was a distributed computing project undertaken by Intelligence Realm, Inc. with the long-term goal of simulating the human brain in real time, complete with artificial consciousness and artificial general intelligence. They claimed to have found, in research, the "mechanisms of knowledge representation in the brain which is equivalent to finding artificial intelligence", before moving into the developmental phase.',
+                recentPapers: [],
+                relativeAuthors: [],
+            };
+        },
+        computed: {
+            keyword () {
+                return {type: this.type, term: this.researchDomain};
+            }
+        },
+        methods: {
+            getActiveAuthorsOfTerm () {
+                let limit = 5;
+                this.$get("/statistics/activeAuthorsOfTerm", {
+                    termId: this.keywordId,
+                    limit: limit
+                }).then(r => {
+                    if (r.data.status) {
+                        this.relativeAuthors = r.data.result;
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 500);
+                    } else {
+                        this.$message({
+                            showClose: true,
+                            message: r.data.result,
+                            type: "warning"
+                        });
+                        this.loading = false;
+                    }
+                }).catch(e => {
+                    this.loading = false;
+                });
+            },
+            cellStyle () {
+                return "color: rgba(0, 30, 135, 0.6); font-size: 18px; cursor: pointer;";
+            },
+            openAuthorOrAffiliation (row, col) {
+                if (col.label !== "所在机构") {
+                    this.openDetailPage("author", row.id);
+                } else {
+                    this.openDetailPage("affiliation", row.affiliationId)
+                }
+            },
+            openDetailPage (detailType, queryId) {
+                const detailPath = {
+                    author: "/authorDetail",
+                    keyword: "/keywordDetail",
+                    affiliation: "/affiliationDetail",
+                    article: "/articleDetail"
+                };
+                this.$router.push({
+                    path: detailPath[detailType],
+                    query: {
+                        id: queryId
+                    }
+                });
+            }
+        },
+        created () {
+            this.keywordId = this.$route.query.id;
+        },
+        mounted () {
+            this.getActiveAuthorsOfTerm();
+        }
+    };
 </script>
 
 <style lang="less" scoped>
-@head-font-size:30px;
-@title-font-size:24px;
-.main-container{
-    .domain-content{
-      
-      .domain-title{
-        font-size: @head-font-size;
-        line-height: 36px;
-        margin-bottom: 20px;
-      }
-      .domain-description{
-        overflow: auto;
-        height: 100px;
-        font-size: 14px;
-      }
-    .domain-recommend{
-      
+    @head-font-size: 30px;
+    @title-font-size: 24px;
+    .main-container {
+        .domain-content {
+
+            .domain-title {
+                font-size: @head-font-size;
+                line-height: 36px;
+                margin-bottom: 20px;
+            }
+
+            .domain-description {
+                overflow: auto;
+                height: 100px;
+                font-size: 14px;
+            }
+
+            .domain-recommend {
+
+            }
+        }
+
+        .info-container {
+            margin-bottom: 20px;
+
+            .card-head-title {
+                font-size: @title-font-size;
+                flex: 1;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                text-align: center;
+            }
+
+            .info-infinite-container {
+                border: 1px solid #e8e8e8;
+                border-radius: 4px;
+                overflow: auto;
+                padding: 8px @title-font-size;
+                padding-top: 0;
+                height: 400px;
+            }
+        }
     }
-    }
-    .info-container{
-      margin-bottom: 20px;
-    .card-head-title {
-      font-size: @title-font-size;
-      flex: 1;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      text-align: center;
-    }
-    .info-infinite-container {
-      border: 1px solid #e8e8e8;
-      border-radius: 4px;
-      overflow: auto;
-      padding: 8px @title-font-size;
-      padding-top: 0;
-      height: 400px;
-    }
-    }
-}
 </style>
